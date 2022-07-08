6256e83b118f7f0235ea512db3847b7fcc8c736c
Iionsroar <yawned.eyes@gmail.com>
2022-07-08 14:36:04 +0800

    cant load plugin fix: https://stackoverflow.com/questions/62688256/sqlalchemy-exc-nosuchmoduleerror-cant-load-plugin-sqlalchemy-dialectspostgre

--- inquizitor/core/config.py
+++ inquizitor/core/config.py
@@ -35,7 +35,7 @@ class Settings(BaseSettings):
 	# 	SQLALCHEMY_DATABASE_URI = "sqlite:///inquizitor/data.db"
 	# else:
 	# SQLALCHEMY_DATABASE_URI: str = os.getenv('DATABASE_URL')
-	SQLALCHEMY_DATABASE_URI: str = "postgres://obdyuztuyoxqsl:180caeb9d6bf7d81960e8665da3820ad7bbeabcb40ae956967dab8003b3e6d47@ec2-34-239-241-121.compute-1.amazonaws.com:5432/d3ujdu5qfskio4"
+	SQLALCHEMY_DATABASE_URI: str = "postgresql://obdyuztuyoxqsl:180caeb9d6bf7d81960e8665da3820ad7bbeabcb40ae956967dab8003b3e6d47@ec2-34-239-241-121.compute-1.amazonaws.com:5432/d3ujdu5qfskio4"
 	# SQLALCHEMY_DATABASE_URI = f"postgresql://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{POSTGRES_SERVER}:{POSTGRES_PORT}/{POSTGRES_DB}"
 
 	EMAIL_TEST_USER: EmailStr = "test@example.com" # type: ignore
